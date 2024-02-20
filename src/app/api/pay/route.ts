36e2cd88de661b171d4830fe2147f02bb1316110
vitalii-f <vit.vit.00.000@gmail.com>
2024-02-20 15:48:01 +0200

    Payment system verify

--- src/app/api/pay/route.ts
+++ src/app/api/pay/route.ts
@@ -2,7 +2,22 @@ export async function POST(request: Request) {
   const message = await request.json();
   console.log(request.headers.get('x-sign'));
   console.log(message);
+  const crypto = require('crypto');
 
+  let pubKeyBase64 = process.env.X_SIGN!;
+  let xSignBase64 = request.headers.get('x-sign');
+
+  if (xSignBase64) {
+    let signatureBuf = Buffer.from(xSignBase64, 'base64');
+    let publicKeyBuf = Buffer.from(pubKeyBase64, 'base64');
+    let verify = crypto.createVerify('SHA256');
+
+    verify.write(message);
+    verify.end();
+
+    let result = verify.verify(publicKeyBuf, signatureBuf);
+    console.log(result === true ? 'OK' : 'NOT OK');
+  }
   return new Response('200 OK', {
     status: 200,
     headers: { 'X-Sign': process.env.X_SIGN! },
